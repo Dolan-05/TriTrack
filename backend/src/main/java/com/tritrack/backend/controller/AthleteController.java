acc8074394cd6e9d917703e7127f343b22d030e8
Dolan-05 <liamdolan106@gmail.com>
2026-09-02 18:24:14 +0100

    Add Athlete controller and service layers

---
+++ backend/src/main/java/com/tritrack/backend/controller/AthleteController.java
@@ -0,0 +1,29 @@
+package com.tritrack.backend.controller;
+
+import com.tritrack.backend.athlete.Athlete;
+import com.tritrack.backend.service.AthleteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/athletes")
+public class AthleteController {
+    private final AthleteService athleteService;
+
+    public AthleteController(AthleteService athleteService){
+        this.athleteService = athleteService;
+    }
+    @GetMapping
+    public String Test(){
+        return "TriTrack API is running and running well!";
+
+    }
+    @PostMapping
+    public Athlete createAthlete(@RequestBody Athlete athlete){
+        return athleteService.saveAthlete(athlete);
+    }
+
+}
---
+++ backend/src/main/java/com/tritrack/backend/service/AthleteService.java
@@ -0,0 +1,18 @@
+package com.tritrack.backend.service;
+
+import com.tritrack.backend.athlete.Athlete;
+import com.tritrack.backend.repository.AthleteRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AthleteService {
+
+    private final AthleteRepository athleteRepository;
+
+    public AthleteService(AthleteRepository athleteRepository) {
+        this.athleteRepository = athleteRepository;
+    }
+    public Athlete saveAthlete(Athlete athlete){
+        return athleteRepository.save(athlete);
+    }
+}
